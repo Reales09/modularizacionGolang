d07e02cf2e5d1c80761da63eb71ca6ce640f033b
Reales Myles <rclarence123@gmail.com>
2023-05-10 12:37:27 -0500

    Instalar modulos de terceros

--- main.go
+++ main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"paquetes/models"
+	"github.com/donvito/hellomod"
 )
 
 func main() {
+
+	hellomod.SayHello()
 	/*
-		mensajes.Hola()
-		mensajes.ImpriimirMensaje()
 
-		cua := figuras.Cuadrado{Lado: 10}
-		figuras.Medidas(&cua)
+			mensajes.Hola()
+			mensajes.ImpriimirMensaje()
+
+			cua := figuras.Cuadrado{Lado: 10}
+			figuras.Medidas(&cua)
+
+
+		p1 := models.Persona{}
+		p1.Constructor("Reales", 32)
+
+		fmt.Println(p1.GetNombre())
+
+		p1.SetNombre("Rectoro")
+
+		fmt.Println(p1.GetNombre())
 	*/
-
-	p1 := models.Persona{}
-	p1.Constructor("Reales", 32)
-
-	fmt.Println(p1.GetNombre())
-
-	p1.SetNombre("Rectoro")
-
-	fmt.Println(p1.GetNombre())
 
 }

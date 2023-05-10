471ea1c9a5e168abed910f95af6ea1e732c09ec9
Reales Myles <rclarence123@gmail.com>
2023-05-10 12:20:37 -0500

    Emulando un constructor

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	"paquetes/figuras"
-	"paquetes/mensajes"
+	"fmt"
+	"paquetes/models"
 )
 
 func main() {
-	mensajes.Hola()
-	mensajes.ImpriimirMensaje()
+	/*
+		mensajes.Hola()
+		mensajes.ImpriimirMensaje()
 
-	cua := figuras.Cuadrado{Lado: 10}
-	figuras.Medidas(&cua)
+		cua := figuras.Cuadrado{Lado: 10}
+		figuras.Medidas(&cua)
+	*/
+
+	p1 := models.Persona{}
+	p1.Constructor("Reales", 32)
+
+	fmt.Println(p1)
+
 }
